7aa3c06c5e99a86fa2e5b943306b9c5a2299ce1a
Carlos Roberto <crpu4a@gmail.com>
2024-10-13 18:57:27 -0300

    first project "Zero to RavenDB" from "Inside RavenDB 4.0"

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Raven.Client.Documents;
+using Rvn.Ch02;
+
+var store = new DocumentStore()
+{
+    Urls = ["http://localhost:8080"],
+    Database = "Tasks"
+};
+
+store.Initialize();
+
+using (var session = store.OpenSession())
+{
+    var task = new ToDoTask()
+    {
+        DueDate = DateTime.Now.AddDays(1),
+        Task = "Nada de mais"
+    };
+    
+    session.Store(task);
+    session.SaveChanges();
+}
+
+using (var session = store.OpenSession())
+{
+    var task = session.Load<ToDoTask>("ToDoTasks/1-A");
+    task.Completed = true;
+    session.SaveChanges();
+}
+
+using (var session = store.OpenSession())
+{
+    for (var i = 0; i < 5; i++)
+    {
+        session.Store(new ToDoTask
+        {
+            DueDate = DateTime.Today.AddDays(i),
+            Task = "Take the dog for a walk"
+        });
+    }
+
+    session.SaveChanges();
+}
+
+
+using (var session = store.OpenSession())
+{
+    var taskToDo =
+        from t in session.Query<ToDoTask>()
+        where t.DueDate >= DateTime.Today &&
+              t.DueDate <= DateTime.Today.AddDays(2) &&
+              t.Completed == false
+        orderby t.DueDate
+        select t;
+
+    Console.WriteLine(taskToDo.ToString());
+
+    foreach (var task in taskToDo)
+    {
+        Console.WriteLine($"{task.Id} - {task.Task} - {task.DueDate}");
+    }
+}
+
+using (var session = store.OpenSession())
+{
+    var tasksPerDay =
+        from t in session.Query<ToDoTask>()
+        group t by t.DueDate
+        into g
+        select new
+        {
+            DueDate = g.Key,
+            TasksPerDate = g.Count()
+        };
+
+    Console.WriteLine(tasksPerDay.ToString());
+
+    foreach (var tpd in tasksPerDay)
+    {
+        Console.WriteLine($"{tpd.DueDate} - {tpd.TasksPerDate}");
+    }
+}
---
+++ ToDoTask.cs
@@ -0,0 +1,9 @@
+namespace Rvn.Ch02;
+
+public class ToDoTask
+{
+    public string Id { get; set; }
+    public string Task { get; set; }
+    public bool Completed { get; set; }
+    public DateTime DueDate { get; set; }
+}
